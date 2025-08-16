6245b730370ad7baa8e1324b4eec219475ad625b
gosanbecasdeal-cyber <gosanbecasdeal@gmail.com>
2025-08-16 13:07:14 +0200

    va ha github

--- back_end/obj/Debug/net8.0/back_end.AssemblyInfo.cs
+++ back_end/obj/Debug/net8.0/back_end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4db819f0c27007b72a1cf7ba7e82d3b59ccf2a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b943f922b3d80f652390c9c0f909961764136442")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
